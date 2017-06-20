12f8304e8a5cce8bba3fb13dfb81bcc2423340d7
oliver <oliver@localhost.localdomain>
2017-06-19 17:39:25 -0700

    deleted superfluous files

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from ez_setup import use_setuptools
-use_setuptools()
+#from ez_setup import use_setuptools
+#use_setuptools()
 from setuptools import setup, find_packages
+#from distutils.core import setup
 
 setup(name='atomicform',
       version='1.0',
-      py_modules=['atomicform'],
+      packages = find_packages('.'),
+      package_dir={'atomicform': 'atomicform'},
       package_data={
             'atomicform': ['data/*'],
          },
+      zip_safe = False
       )
 
